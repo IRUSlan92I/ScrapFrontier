ae59ef5669058fe714972be4cb8ce838836cfd78
Ruslan Ignatov <ruslanlena30@gmail.com>
2025-10-20 22:23:46 +0300

    Removed debug print

--- game/entities/player.gd
+++ game/entities/player.gd
@@ -10,7 +10,6 @@ var position : Vector2:
 
 func _process(delta: float) -> void:
 	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	print(input_direction)
 	
 	if input_direction.is_zero_approx():
 		$Ship.decelerate($Ship.deceleration * delta)
