67342c82ccf31a48a2ef8894ceedda2aa92be7ec
Ruslan Ignatov <ruslanlena30@gmail.com>
2025-11-29 21:19:30 +0300

    Added background

--- game/passage.gd
+++ game/passage.gd
@@ -5,11 +5,17 @@ extends Node2D
 signal player_died
 
 
-const PLAYER := preload("res://game/entities/ships/player/player_ship.tscn")
-
-
 @onready var enemy_swamp_controller : EnemySwampController = $EnemySwampController
 @onready var enemy_timer : Timer = $EnemyTimer
+@onready var paralax_1 : Parallax2D = $Background/Parallax1
+@onready var paralax_2 : Parallax2D = $Background/Parallax2
+@onready var paralax_3 : Parallax2D = $Background/Parallax3
+
+
+func _ready() -> void:
+	paralax_1.scroll_offset.x = randf_range(1, paralax_1.repeat_size.x)
+	paralax_2.scroll_offset.x = randf_range(1, paralax_2.repeat_size.x)
+	paralax_3.scroll_offset.x = randf_range(1, paralax_3.repeat_size.x)
 
 
 func _on_enemy_timer_timeout() -> void:
