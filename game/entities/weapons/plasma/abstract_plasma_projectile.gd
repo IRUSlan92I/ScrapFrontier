309112a1f21df7ee301745cf5b2f7a466cd16761
Ruslan Ignatov <ruslanlena30@gmail.com>
2025-12-22 20:54:45 +0300

    Updated plasma projectile

--- game/entities/weapons/plasma/abstract_plasma_projectile.gd
+++ game/entities/weapons/plasma/abstract_plasma_projectile.gd
@@ -1,2 +1,13 @@
 class_name AbstractPlasmaProjectile
 extends AbstractDirectHitProjectile
+
+
+const ANIMATION_NAME = "default"
+
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	super._ready()
+	sprite.play(ANIMATION_NAME)
